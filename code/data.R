37c85c00f89401eb58318428072087c27e4cda32
Susan Paykin <susan.paykin@gmail.com>
2023-07-12 16:54:48 -0500

    analysis

--- code/data.R
+++ code/data.R
@@ -1,8 +1,10 @@
 library(tidyverse)
-library(vtable)
 library(DescTools)
 library(DT)
 library(stargazer)
+library(vtable)
+
+setwd("~/Documents/GitHub/eval-report")
 
 data <- read.csv("data/Partner DMA Scores + Responses.csv")
 head(data)
@@ -33,6 +35,9 @@ sum(grepl("Gender Equality", data$What.field.does.your.organization.primarily.wo
 sum(grepl("Racial and Social Justice", data$What.field.does.your.organization.primarily.work.in.))
 sum(grepl("Other", data$What.field.does.your.organization.primarily.work.in.))
 
+
+#### Budget ####
+
 budget <- data %>% 
   select(1,9,63:76) %>%
   group_by(What.is.your.organization.s.annual.operating.budget..in.U.S..dollars..) %>%
@@ -43,8 +48,25 @@ budget
 datatable(budget)
 
 budget_transpose = t(budget)
-write.csv(budget_transpose, "budget.csv")
+write.csv(budget_transpose, "outputs/budget.csv")
 
+#### Budget <1M ####
+less10M <- data %>% 
+  select(1,9,63:76) %>%
+  group_by(What.is.your.organization.s.annual.operating.budget..in.U.S..dollars..) %>%
+  filter(!What.is.your.organization.s.annual.operating.budget..in.U.S..dollars.. %in% 
+           c("$5 million to $9.9 million", "$10 million to $24.9 million")) %>%
+  summarise_at(.vars = names(.)[3:16],.funs = c(mean="mean"))
 
+less10M_t = t(less10M)
+write.csv(less10M_t, "outputs/budgetLess10M.csv")
 
-  
+#### Number of Employees ####
+
+staff <- data %>%
+  select(1,10,63:76) %>%
+  group_by(How.many.employees.work.at.your.organization.) %>%
+  summarise_at(.vars = names(.)[3:16],.funs = c(mean="mean"))
+
+staff_t <- t(staff)
+write.csv(staff_t, "outputs/staff.csv")  
